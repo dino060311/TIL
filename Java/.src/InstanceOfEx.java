ccac24eb3f7cf984aa41f93c67451991f263a332
dino060311 <ella060311@gmail.com>
2026-05-19 11:49:16 +0900

    feat: instanceof 연산자 활용 실습 코드 추가

---
+++ Java/.src/InstanceOfEx.java
@@ -0,0 +1,27 @@
+class Person1 { }
+class Student1 extends Person1 { }
+class Researcher extends Person1 { }
+class Professor extends Researcher { }
+
+public class InstanceOfEx {
+    static void print(Person1 p) {
+        if (p instanceof Person1)
+            System.out.print("Person1 ");
+        if (p instanceof Student1)
+            System.out.print("student1 ");
+        if (p instanceof Researcher)
+            System.out.print("Researcher ");
+        if (p instanceof Professor)
+            System.out.print("Professor ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.print("new Student1() ->\t");
+        print(new Student1());
+        System.out.print("new Researcher() ->\t");
+        print(new Researcher());
+        System.out.print("new Professor() ->\t");
+        print(new Professor());
+    }
+}
